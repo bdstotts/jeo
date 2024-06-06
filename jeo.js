55d52cedd89ae7aa16745d7b299d524bbc63919a
Brody Stotts <150973033+bdstotts@users.noreply.github.com>
2024-06-06 16:18:48 -0600

    add third category button and accompanying logic

--- jeo.js
+++ jeo.js
@@ -2,6 +2,7 @@
 const scoreBoard = document.getElementById("scoreBoard");
 let teamData = []
 var teams = ""
+catTitlesAll = []
 
 
 catBoard = document.getElementById("catBoard")
@@ -39,6 +40,7 @@ const cats1 = [//It all adds up
     {dollars: "$400", link: "https://www.youtube.com/watch?v=AdQ3JDLlmPI&ab_channel=GameofThrones", answer:"GoT"},
     {dollars: "$500", link: "https://www.youtube.com/watch?v=O8W9qr7DB-o&ab_channel=GilGonzalez", answer:"twd"}],
 ]
+catTitlesAll.push(catTitles1)
 
 const catTitles2 = ["Colors of the Rainbow", "Divas","Boy Bands", "Letters to You","A Trip Down Alternative Lane"]
 
@@ -73,6 +75,44 @@ const cats2 = [//Colors of the Rainbow
     {dollars: "$800", link:  "https://www.youtube.com/watch?v=TD6V9Vz9yTY&ab_channel=VibersVibin", answer:"Sum 41 in too deep"},
     {dollars: "$1000", link: "https://www.youtube.com/watch?v=NDHY1D0tKRA&ab_channel=Truhffles", answer:"Deathcab ill follow you into the dark"}],
 ]
+catTitlesAll.push(catTitles2)
+
+const catTitles3 = ["Cat1", "Cat2","Cat3", "Cat4","Cat5"]
+
+const cats3 = [//Colors of the Rainbow
+    [{dollars:"$200", link: "https://www.youtube.com/watch?v=Y7ix6RITXM0&ab_channel=Maroon5VEVO", answer:"Maroon 5 Maps"},
+    {dollars: "$400", link: "https://www.youtube.com/watch?v=51XzW98wEDg&ab_channel=7clouds", answer:"Green Day boulevard of broken"},
+    {dollars: "$600", link: "https://www.youtube.com/watch?v=0LMSF4tbHQQ&ab_channel=WindMusic", answer:"Black Sabbath Iron Man"},
+    {dollars: "$800", link: "https://www.youtube.com/watch?v=dOLBn8GKBlA&ab_channel=daavve95", answer:"Blue MAn group drumbone"},
+    {dollars: "$1000", link: "https://www.youtube.com/watch?v=Dy4HA3vUv2c&ab_channel=BlueOysterCultVEVO", answer:"Blue oyster cult ... reaper"}],
+    //Divas..........................................................................
+    [{dollars:"$200", link: "https://www.youtube.com/watch?v=HmA7vHGH1wI&ab_channel=VibeMusic", answer:"Celine Dion MY heart will go on"},
+    {dollars: "$400", link: "https://www.youtube.com/watch?v=NrJEFrth27Q&ab_channel=MariahCareyVEVO", answer:"MAriah Carrey Emotions"},
+    {dollars: "$600", link: "https://www.youtube.com/watch?v=t4noaywbaik&ab_channel=Alternate", answer:"Whitney Houston I wanna dance with somebody"},
+    {dollars: "$800", link: "https://www.youtube.com/watch?v=wekDNXDWGjM&ab_channel=VibeMusic", answer:"Beyonce Halo"},
+    {dollars: "$1000", link: "https://www.youtube.com/watch?v=QUme0h-uPP4&ab_channel=SyrebralVibes", answer:"Ariana G Thank you next"}],
+    //Boy Bands..........................................................................
+    [{dollars:"$200", link: "https://www.youtube.com/watch?v=aS-cymQhbzk&ab_channel=NSYNCVEVO", answer:"Nsync space cowboy"},
+    {dollars: "$400", link: "https://www.youtube.com/watch?v=7JFzuQtSxh4&ab_channel=Pillow", answer:"backstreet boys largr than life"},
+    {dollars: "$600", link: "https://www.youtube.com/watch?v=mfavkCY52MI&ab_channel=MSCLyrics", answer:"One Direction what makes you beautiful"},
+    {dollars: "$800", link: "https://www.youtube.com/watch?v=Rj17ICdkUxo&ab_channel=ALBAMUSIC", answer:"98 deg beacsue of you"},
+    {dollars: "$1000", link: "https://www.youtube.com/watch?v=o9mLyHtSLjk&ab_channel=BTS-Topic", answer:"BTS Butter"}],
+     //Letters to You..........................................................................
+     [{dollars:"$200", link: "https://www.youtube.com/watch?v=skwZ5MQ7CfE&ab_channel=Vibratium", answer:"ACDC thunderstruk"},
+     {dollars: "$400", link: "https://www.youtube.com/watch?v=EzeDqRhM09w&ab_channel=killingloneleyness01", answer:"U2"},
+     {dollars: "$600", link: "https://www.youtube.com/watch?v=5IqkhhSycc4", answer:"REM"},
+     {dollars: "$800", link: "https://www.youtube.com/watch?v=tsMDKjb54EM&ab_channel=Loku", answer:"LMFAO party rock anthem"},
+     {dollars: "$1000", link: "https://www.youtube.com/watch?v=d1Nq82S7_0Q&ab_channel=TheB-52%27s-Topic", answer:"B-52s"}],
+      //Classic Emo..........................................................................
+    [{dollars:"$200", link: "https://www.youtube.com/watch?v=iBeyEXd46U8&ab_channel=TheSquiggleyPanda", answer:"Yellow Card Ocean Avenue"},
+    {dollars: "$400", link:  "https://www.youtube.com/watch?v=SU9Fm2bqZTo&ab_channel=21CLyrics", answer:"Fallout boy sugar were going.."},
+    {dollars: "$600", link:  "https://www.youtube.com/watch?v=4QWzYjatCCs&ab_channel=B3AST858", answer:"Jimmy Eat World The middle"},
+    {dollars: "$800", link:  "https://www.youtube.com/watch?v=TD6V9Vz9yTY&ab_channel=VibersVibin", answer:"Sum 41 in too deep"},
+    {dollars: "$1000", link: "https://www.youtube.com/watch?v=NDHY1D0tKRA&ab_channel=Truhffles", answer:"Deathcab ill follow you into the dark"}],
+]
+
+catTitlesAll.push(catTitles3)
+console.log(catTitlesAll)
 
 
 createBoard(catTitles1,cats1);
@@ -178,18 +218,34 @@ for(i=1;i<parseInt(teams)+1;i++){
 team1Box = document.getElementById("team1Box")
 const doubleJep = document.createElement("div")
 doubleJep.classList.add("double")
-doubleJep.addEventListener("click", (destroyRebuild))
+doubleJep.addEventListener("click", (destroyRebuild2))
 mediaBox.appendChild(doubleJep);
 
-function destroyRebuild(){
+const tripleJep = document.createElement("div")
+tripleJep.classList.add("triple")
+tripleJep.addEventListener("click", (destroyRebuild3))
+mediaBox.appendChild(tripleJep);
+
+function destroy(){
     squares = document.getElementsByClassName("square");
     squares = Array.from(squares);
     squares.forEach(element => {
     element.remove();        
     });
 
+}
+
+// functions for removing current clue squares and adding new ones.. needs refactoring. 
+
+function destroyRebuild2(){
+    destroy()
     createBoard(catTitles2, cats2)
 
+}
+
+function destroyRebuild3(){
+    destroy()
+    createBoard(catTitles3,cats3)
 }
 
 
